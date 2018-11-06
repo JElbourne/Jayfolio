d46b081139270c1daac9fd3aceab9421b5a600ab
Jay Elbourne <jayelbourne@gmail.com>
2018-11-05 19:03:06 -0500

    Added a new Service layer and added a new interface for Projects.

---
+++ Jayfolio.Data/IProject.cs
@@ -0,0 +1,19 @@
+﻿using Jayfolio.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jayfolio.Data
+{
+    public interface IProject
+    {
+        Project GetById(int _id);
+        IEnumerable<Project> GetAll();
+
+        Task Create(Project _project);
+        Task Delete(int _projectId);
+        Task UpdateProjectTitle(int _projectId, string _newTitle);
+        Task UpdateProjectDescription(int _porjectId, string _newDescription);
+    }
+}
---
+++ Jayfolio.Service/ProjectService.cs
@@ -0,0 +1,50 @@
+﻿using Jayfolio.Data;
+using Jayfolio.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Jayfolio.Service
+{
+    public class ProjectService : IProject
+    {
+        private readonly ApplicationDbContext m_context;
+
+        public ProjectService(ApplicationDbContext _context)
+        {
+            m_context = _context;
+        }
+
+        public Task Create(Project _project)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int _projectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Project> GetAll()
+        {
+            return m_context.Projects
+                .Include(project => project.Posts);
+        }
+
+        public Project GetById(int _id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateProjectDescription(int _porjectId, string _newDescription)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateProjectTitle(int _projectId, string _newTitle)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
