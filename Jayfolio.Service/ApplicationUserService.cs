f55885c001fca01b8f3ba6fce6ef904565079ed5
Jay Elbourne <jayelbourne@gmail.com>
2018-11-23 20:13:35 -0500

    Create Profile Controller, AppUser, Upload Services

---
+++ Jayfolio.Service/ApplicationUserService.cs
@@ -0,0 +1,38 @@
+﻿using Jayfolio.Data;
+using Jayfolio.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jayfolio.Service
+{
+    class ApplicationUserService : IApplicationUser
+    {
+        private readonly ApplicationDbContext m_context;
+
+        public ApplicationUserService(ApplicationDbContext _context)
+        {
+            m_context = _context;
+        }
+
+        public IEnumerable<ApplicationUser> GetAll()
+        {
+            return m_context.ApplicationUsers;
+        }
+
+        public ApplicationUser GetById(string id)
+        {
+            return GetAll().FirstOrDefault(
+                user => user.Id == id);
+        }
+
+        public async Task SetProfileImage(string id, Uri uri)
+        {
+            var user = GetById(id);
+            user.ProfileImageUrl = uri.AbsoluteUri;
+            m_context.Update(user);
+            await m_context.SaveChangesAsync();
+        }
+    }
+}
---
+++ Jayfolio.Web/Controllers/ProfileController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Jayfolio.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jayfolio.Web.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly UserManager<ApplicationUser> m_userManager;
+        private readonly IApllicationUser m_userService;
+        private readonly IUpload m_uploadService;
+
+        public ProfileController(
+            UserManager<ApplicationUser> _userManager,
+            IApplicationUser _userService,
+            IUpload _uploadService
+            )
+        {
+            m_uploadService = _uploadService;
+            m_userService = _userService;
+            m_userManager = _userManager;
+        }
+
+        public IActionResult Detail(string id)
+        {
+            return View();
+        }
+    }
+}
